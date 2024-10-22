5aea9c4699654ca20c295d00b004c8afc74433ff
Diego Fabbri <diegofabbri.dev@gmail.com>
2024-10-22 17:36:47 -0300

    feat: add user entity

---
+++ src/main/java/com/picpaySimplificado/domain/user/User.java
@@ -0,0 +1,39 @@
+package com.picpaySimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String firstName;
+
+  private String lastName;
+
+  @Column(unique = true)
+  private String document;
+
+  @Column(unique = true)
+  private String email;
+
+  private String password;
+
+  private BigDecimal balance;
+
+  @Enumerated(EnumType.STRING)
+  private UserType userType;
+}
